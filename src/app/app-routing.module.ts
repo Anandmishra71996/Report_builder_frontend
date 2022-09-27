79514b245b73f5a850728dc42932e691a794f75e
unknown <anand.mishra@oodles.io>
2022-09-27 12:03:27 +0530

    bug fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './Components/landing-page/landing-page.component';
+import { VerifyPasswordComponent } from './account/verify-password/verify-password.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:LandingPageComponent},
+  {path:'verifyPassword/:id',component:VerifyPasswordComponent},
+  {
+    path:'admin',
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
+  },
+  {
+    path:'account',
+    loadChildren: () => import('./account/account.module').then(m => m.AccountModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from "@angular/common/http";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { LandingPageComponent } from './Components/landing-page/landing-page.component';
+import { SharedModule } from './shared/shared.module';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    SharedModule,
+    BrowserAnimationsModule,
+    ModalModule.forRoot(),
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
